cc1dbf86e0ba0b7f41f97d65cf4e41be3465e774
Félix  Merdinger <merdinger.felix@gmail.com>
2022-06-14 16:05:42 +0200

    level6.c

--- level6/source.c
+++ level6/source.c
@@ -11,15 +11,12 @@ void m(void)
 }
 
 
-void main(undefined4 placeholder_0, char **envp)
+int main(int argc, char **argv)
 {
-    undefined4 uVar1;
-    code **ppcVar2;
-    
-    uVar1 = malloc(0x40);
-    ppcVar2 = (code **)malloc(4);
-    *ppcVar2 = m;
-    strcpy(uVar1, envp[1]);
-    (**ppcVar2)();
-    return;
+    string = malloc(0x40);
+    func = (code **)malloc(4);
+    *func = m;
+    strcpy(string, argv[1]);
+    func();
+    return 0;
 }
