ea93d3781e4c99b4c1e5487efd8c82a9c6e5f034
Felix MERDINGER <fmerding@e2r9p10.42.fr>
2022-06-18 13:26:13 +0000

    bonus33

--- bonus3/source.c
+++ bonus3/source.c
@@ -10,14 +10,14 @@ int main(int argc, char **argv)
     file = fopen("/home/user/end/.pass", "r");
     bzero(buffer1);
     if ((!file) || (argc != 2))
-        return -1
+        return -1;
     fread(buffer1, 1, 66, file);
     int nb = atoi(argv[1]);
     buffer1[nb] = 0;
-    fread(buffer2[66], 1, 65, file);
+    fread(buffer2, 1, 65, file);
     fclose(file);
     int isSame = strcmp(buffer1, argv[1]);
-    if (isSame == 0) 
+    if (isSame == 0)
         execl("/bin/sh", "sh", 0);
     else 
         puts(buffer2);
